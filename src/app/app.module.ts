02fbf0e1c42e5e29f4b88834065e645f38fbbdfe
Illia Andreiev <ilya.andreev.it@gmail.com>
2018-10-22 15:20:24 +0200

    Basic components generated Basic routing configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
+import { MaterialModule } from './material/material.module';
 import { MainNavComponent } from './main-nav/main-nav.component';
+import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { SupportContactComponent } from './support-contact/support-contact.component';
+
+const appRoutes: Routes = [
+  { path: 'add-recipe', component: AddRecipeComponent},
+  { path: 'recipes', component: RecipesComponent },
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'support-contact', component: SupportContactComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainNavComponent
+    MainNavComponent,
+    AddRecipeComponent,
+    RecipesComponent,
+    CategoriesComponent,
+    SupportContactComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
