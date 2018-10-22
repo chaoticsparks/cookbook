301fb2e79058a3ceb29a76983033a761b65340ca
Illia Andreiev <ilya.andreev.it@gmail.com>
2018-10-22 15:04:39 +0200

    Initial styles and markups for Index page

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,46 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatIconModule } from '@angular/material';
+import {
+  MatButtonModule, MatCardModule,
+  MatChipsModule,
+  MatFormFieldModule,
+  MatIconModule, MatInputModule,
+  MatListModule, MatRadioModule, MatSelectModule,
+  MatSidenavModule, MatSlideToggleModule,
+  MatToolbarModule
+} from '@angular/material';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule],
-  exports: [MatButtonModule, MatIconModule],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatChipsModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatSlideToggleModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatChipsModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatSlideToggleModule
+  ],
 })
 export class MaterialModule {
 }
